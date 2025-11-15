b823c80a6282ea2beaf7dd702bf91a70d885d2a7
cabs <carlosbars8@gmail.com>
2025-11-15 07:08:42 -0500

    add wrong v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./Apps/**/*.{js,jsx,ts,tsx}",
-    "./Components/**/*.{js,jsx,ts,tsx}",
-    "./Screens/**/*.{js,jsx,ts,tsx}",
-    "./**/*.{js,jsx,ts,tsx}",
-  ],
-  presets: [require("nativewind/preset")],   // <- OBLIGATORIO
+  content: ["./App.{js,jsx,ts,tsx}","*.{js,jsx,ts,tsx}", "./Apps/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
