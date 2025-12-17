cfc982571d7df5974c351b01189d0f246d4e295e
cabs <carlosbars8@gmail.com>
2025-12-17 11:40:34 -0500

    add sonidos notificav1

--- Apps/Components/Notifications/notificationSetup.jsx
+++ Apps/Components/Notifications/notificationSetup.jsx
@@ -10,9 +10,9 @@ export async function setupBackendNotifications() {
   if (Platform.OS === "android") {
     // Definimos los audios que configuraste en app.json
     const canales = [
-      { id: "v5_alerta_uno", sound: "alerta_uno.wav", name: "Alerta Médica 1 v5" },
-      { id: "v5_alerta_dos", sound: "alerta_dos.wav", name: "Alerta Médica 2 v5" },
-      { id: "v5_alerta_tres", sound: "alerta_tres.wav", name: "Alerta Médica 3 v5" },
+      { id: "v8_alerta_uno", sound: "alerta_uno", name: "Alerta Médica 1 v8" },
+      { id: "v8_alerta_dos", sound: "alerta_dos", name: "Alerta Médica 2 v8" },
+      { id: "v8_alerta_tres", sound: "alerta_tres", name: "Alerta Médica 3 v8" },
     ];
 
     for (const canal of canales) {
